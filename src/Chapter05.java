c309e4589573fe4a85c8dfabebd5e99b6bfde943
MisuzuDate <138319041+maruri6797@users.noreply.github.com>
2023-11-06 17:32:52 +0900

    Chapter5完了

---
+++ src/Chapter05.java
@@ -0,0 +1,15 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String str = "MisuzuDate";
+		System.out.println(str);
+		str = "あいうえお";
+		System.out.println(str);
+		final int number = 7;
+		System.out.println(number);
+
+	}
+
+}
