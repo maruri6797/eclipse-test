a7bedf7d0f6f65235c5df7f6fcd3953caf7ab78e
MisuzuDate <138319041+maruri6797@users.noreply.github.com>
2023-11-08 13:34:33 +0900

    Chapter06完了

---
+++ src/Chapter06.java
@@ -0,0 +1,19 @@
+
+public class Chapter06 {
+	public static void main(String[] args) {
+		long long1 = 10000000000L;
+		System.out.println(long1);
+		double double1 = 3.14;
+		System.out.println(double1);
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		Integer int1 = 100;
+		System.out.println(int1);
+		Float float1 = 1.4142f;
+		System.out.println(float1);
+		Character cha1 = 'う';
+		System.out.println(cha1);
+		int[] array1 = { 0, 10, 20, 30, 40 };
+		System.out.println(array1[3]);
+	}
+}
