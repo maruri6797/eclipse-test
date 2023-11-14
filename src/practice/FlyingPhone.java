b11015b69f27102e21b297129b667746813eee1d
MisuzuDate <138319041+maruri6797@users.noreply.github.com>
2023-11-14 13:12:48 +0900

    Chapter15完了

---
+++ src/practice/FlyingPhone.java
@@ -0,0 +1,26 @@
+package practice;
+
+import extend.phone.Phone;
+
+public class FlyingPhone implements Phone, Flying {
+	private int minutes;
+	
+	public FlyingPhone(int minutes) {
+		this.minutes = minutes;
+	}
+	
+	@Override
+	public void call(String number) {
+		System.out.println(number + "に電話します。通話できるのは飛んでいる間だけです。");
+	}
+	
+	@Override
+	public void fly() {
+		System.out.println(minutes + "分間、飛びます。");
+	}
+	
+	@Override
+	public void powerOff() {
+		Flying.super.powerOff();
+	}
+}
